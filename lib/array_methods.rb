255aca9da836992bfa55fcf0e61b0e204863d56f
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-01-22 06:21:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,14 +8,9 @@ end
 
 
 def find_max_value(array)
-count=0
 max=0
-while count<array.length do
-if array[count] > max
-max = array[count]
-end
-end
-max 
+array.length.times do |count|
+  if array[index]
 end
 
 def find_min_value(array)
